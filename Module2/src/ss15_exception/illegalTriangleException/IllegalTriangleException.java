ddf5410e5c1280a7def7bf68b61356280cbafa5b
duynguyen619 <dragonit662001@gmail.com>
2022-10-25 20:40:36 +0700

    Bai tap

---
+++ Module2/src/ss15_exception/illegalTriangleException/IllegalTriangleException.java
@@ -0,0 +1,7 @@
+package ss15_exception.illegalTriangleException;
+
+public class IllegalTriangleException extends Throwable {
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
+}
